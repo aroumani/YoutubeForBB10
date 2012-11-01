5faa5a334559a7cffb987b230d6d184b28c1886e
adib <adib.roumani@gmail.com>
2012-11-01 01:49:37 -0400

    adding core changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $( document ).bind( "mobileinit", function() {
     
     
 });
-
+var info=null;
 var player=null;
 function pageLoad(){
       
@@ -17,13 +17,84 @@ function pageLoad(){
 		player.stopVideo();
 	});
 	
+	 $(document).ajaxError(function(event, request, setting) 
+{ 
+                    alert('fuckk...');
+         }); 
 	//player.loadVideoById(videoId:String, startSeconds:Number, suggestedQuality:String)
 	//player.stopVideo()
 }
 
 function loadVideo(videoID){
+
+//wA4ppvp2IzY
+
+//http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v=3U72hzeBLOw
+//http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v=wA4ppvp2IzY&r=1351652598093
+//http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch%3Fv%3DwA4ppvp2IzY&xy=yx&bf=false&r=1351652598093
+//wA4ppvp2IzY
+
+//http://www.youtube-mp3.org/api/itemInfo/?video_id=wA4ppvp2IzY&ac=www&r=1351652598222
+//info = { "title" : "One and Only - Adele (Lyrics)", "image" : "http://i.ytimg.com/vi/wA4ppvp2IzY/default.jpg", "length" : "5", "status" : "serving",  "progress_speed" : "",  "progress" : "",  "ads" : "",  "pf" : "http://d27e.u.aclst.com/ping.php?video_id=wA4ppvp2IzY&h=1040",  "h" : "8e61d58bf57d57021c631c02a219af3b"  }
+
+//http://www.youtube-mp3.org/get?video_id=wA4ppvp2IzY&h=8e61d58bf57d57021c631c02a219af3b&r=1351652598334
+
 	player.loadVideoById(videoID);//, startSeconds:Number, suggestedQuality:String)
+	
+	return true;
 }
+
+function download(videoID){
+
+
+	
+	$.mobile.allowCrossDomainPages = true;
+	//queue converstion
+	try{
+		
+		$.ajax({
+		  aysnc:true,
+		  crossDomain: true,
+		  dataType: 'jsonp',
+		  url: 'http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v='+videoID,
+		  error: function(xhr,status,err){
+			$.getScript('http://www.youtube-mp3.org/api/itemInfo/?video_id='+videoID, function() {
+				console.log(info);
+				console.log("http://www.youtube-mp3.org/get?video_id=wA4ppvp2IzY&h="+info.h);
+				if (info != null){
+					setTimeout(function(){
+						window.location="http://www.youtube-mp3.org/get?video_id="+videoID+"&h="+info.h;
+					},3000)
+				}else{
+					alert('Video Cannot Be Downloaded...');
+				}
+				
+			});
+		     
+		 },
+		  success: function(data) {
+		  }
+		});
+	}catch(ex){
+		alert(ex);
+	}
+	return true;
+}
+
+function checkForDownload(videoID, el){
+	$.ajax({
+	  aysnc:true,
+	  crossDomain: true,
+	  dataType: 'jsonp',
+	  url: 'http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v='+videoID,
+	  error: function(xhr,status,err){
+		el.show();
+	 },
+	  success: function(data) {
+	  }
+	});
+}
+
 
 function search(){
 	var q=$("#searchField").val();
@@ -40,11 +111,21 @@ function search(){
 				</a><a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop">Purchase album</a>
 			</li>*/	
 		console.log(video);
-		html += ('<li><a href="#two" onclick="loadVideo(\''+video.videoId+'\');" ><img src="'+video.thumbs[3].url+'" /><h3>'+video.title+'</h3><p></p></a>'+
-			 '<a href="#download" data-rel="popup" data-position-to="window" data-transition="pop">Download MP3</a></li>');
+		html += ('<li data-videoid="'+video.videoId+'" ><a href="#two" onclick="loadVideo(\''+video.videoId+'\');" ><img src="'+video.thumbs[3].url+'" /><h3>'+video.title+'</h3><p></p></a>'+
+			 '<a class="dlLink" href="#popup" data-role="button" data-rel="dialog" data-transition="pop">Download MP#</a></li>');
 	}
 	$("#videoList").html(html);
 	$("#videoList").listview("refresh");
+	
+	$("#videoList").find(".dlLink").hide('slow', function(){
+		checkForDownload($(this).parent("li").jqmData("videoid"), $(this));
 	});
+		
+	$("#videoList a.ui-li-link-alt").live("click", function(e){
+		download($(this).parent("li").jqmData("videoid"));
+	});
+	});
+	
+	
 
 }
