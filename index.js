5c854f9ee210e9bc8bc6d5ed04e8b0bdb07ff767
Adib Roumani <adib.roumani@gmail.com>
2012-11-09 11:52:03 -0500

    Prod Debug Version

--- index.js
+++ index.js
@@ -28,15 +28,16 @@ downloadFile = function(atr, url, fname){
 
     var filePath = window.appRootDir.fullPath + "/" + fname + ".mp3";
 
+	$('#dl_toast').toast('show');
     //alert('try:'+ filePath); 
     fileTransfer.download(
         url,
         filePath,
         function(entry) {
-		alert('Download Complete for: ' + fname + ".mp3");
+		alert('Download Complete for: ' + fname + ".mp3" + ' File saved to: [' + filePath +']');
         },
         function(error) {
-		  alert('An error occurd while trying to download: ' + fname + ".mp3");
+		  alert('An error occurd while trying to download: ' + fname + ".mp3" + ' Attempted to save file saved to: [' + filePath +']');
         }
     );
 }
@@ -58,17 +59,8 @@ $( document ).bind( "mobileinit", function() {
 
 
 var info=null;
-var player=null;
 function pageLoad(){
       
-	
-	player = new YT.Player('player', {
-	});
-	
-	
-	$('#two').live('pagehide',function(event, ui){
-		player.stopVideo();
-	});
 	
 	 $(document).ajaxError(function(event, request, setting) 
 { 
@@ -83,29 +75,8 @@ function pageLoad(){
 	     }
 	}).focus();
 	
-	//player.loadVideoById(videoId:String, startSeconds:Number, suggestedQuality:String)
-	//player.stopVideo()
-	
 }
 
-function loadVideo(videoID){
-
-//wA4ppvp2IzY
-
-//http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v=3U72hzeBLOw
-//http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v=wA4ppvp2IzY&r=1351652598093
-//http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch%3Fv%3DwA4ppvp2IzY&xy=yx&bf=false&r=1351652598093
-//wA4ppvp2IzY
-
-//http://www.youtube-mp3.org/api/itemInfo/?video_id=wA4ppvp2IzY&ac=www&r=1351652598222
-//info = { "title" : "One and Only - Adele (Lyrics)", "image" : "http://i.ytimg.com/vi/wA4ppvp2IzY/default.jpg", "length" : "5", "status" : "serving",  "progress_speed" : "",  "progress" : "",  "ads" : "",  "pf" : "http://d27e.u.aclst.com/ping.php?video_id=wA4ppvp2IzY&h=1040",  "h" : "8e61d58bf57d57021c631c02a219af3b"  }
-
-//http://www.youtube-mp3.org/get?video_id=wA4ppvp2IzY&h=8e61d58bf57d57021c631c02a219af3b&r=1351652598334
-
-	player.loadVideoById(videoID);//, startSeconds:Number, suggestedQuality:String)
-	
-	return true;
-}
 
 function download(atr, videoID){
 
@@ -117,7 +88,7 @@ function download(atr, videoID){
 		$.getScript('http://www.youtube-mp3.org/api/pushItem/?item=http%3A//www.youtube.com/watch?v='+videoID, function() {});
 		//alert('test');
 		$.getScript('http://www.youtube-mp3.org/api/itemInfo/?video_id='+videoID, function() {
-			console.log(info.h);
+			//console.log(info.h);
 			//alert(info.h);
 			if (info != null){
 				//window.open("http://www.youtube-mp3.org/get?video_id="+videoID+"&h="+info.h);
@@ -139,7 +110,7 @@ function checkForDownload(videoID, el){
 		//$.getScript('http://www.youtube-mp3.org/api/itemInfo/?video_id=Ki86x1WKPmE', function() {
 		
 		
-			console.log(info);
+			//console.log(info);
 			//console.log("http://www.youtube-mp3.org/get?video_id=wA4ppvp2IzY&h="+info.h);
 			if (info != null){
 				el.show();
@@ -163,6 +134,8 @@ var cur_song=0;
 function pauseAudio(){
 	if (my_media){
 		my_media.pause();
+		$("#playPauseButtonID").jqmData('icon','arrow-r');
+		$("#playPauseButtonID .ui-icon").addClass("ui-icon-arrow-r").removeClass("ui-icon-delete");
 		mediaPlaying=false;
 	  }
 }
@@ -170,21 +143,40 @@ function pauseAudio(){
 function resumeAudio(){
 	if (my_media){
 		mediaPlaying=true;
+		$("#playPauseButtonID").jqmData('icon','delete');
+		$("#playPauseButtonID .ui-icon").addClass("ui-icon-delete").removeClass("ui-icon-arrow-r");
 		my_media.play();
 	  }
 }
 
-function nextSong(){
+function nextSong(forward){
 	stopAudio();
-	cur_song++;
+	if (forward){
+		cur_song++;
+	}else{
+		cur_song--;
+	}
+	
 	getSong(cur_song, function(json){
-		playAudio(json.path, json.title, cur_song);
+		if (json){
+			playAudio(json.path, json.title, cur_song);
+		}else{
+			alert('end of list reach..');
+			if (forward){
+				cur_song=-1;
+			}else{
+				cur_song=1;
+			}
+			nextSong(forward);
+		}
 	});
 }
 
 function stopAudio(){
 	if (my_media){
 		mediaPlaying=false;
+		$("#playPauseButtonID").jqmData('icon','arrow-r');
+		$("#playPauseButtonID .ui-icon").addClass("ui-icon-arrow-r").removeClass("ui-icon-delete");
 		my_media.stop();
 	  }
 }
@@ -192,7 +184,7 @@ function stopAudio(){
 function playPauseAudio() {
 	if (my_media){
 		if(mediaPlaying){
-			stopAudio();
+			pauseAudio();
 		}else{
 			resumeAudio();
 		}
@@ -214,6 +206,9 @@ function playAudio(src, name, num) {
 		my_media = new Media(src, function(){}, function(){}, function status(constant){
 			if (constant==Media.MEDIA_STOPPED){
 				alert('stoped');
+				if (mediaPlaying){
+					nextSong(true);
+				}
 			}
 		});
 
@@ -224,7 +219,6 @@ function playAudio(src, name, num) {
 				// success callback
 				function(position) {
 					if (position > -1) {
-						console.log((position) + " sec");
 						$('#songSlider').val(""+(position/my_media.getDuration() * 100));
 						$('#songSlider').slider('refresh');
 					}
@@ -263,7 +257,6 @@ function getSong(num, callback){
 	    for (i=0; i<entries.length; i++) {
 			if (i==num){
 				callback("{'path':'"+entries[i].fullPath  +"', 'title':'"+entries[i].name+"'}");
-				return;
 			}
 	    }
 		callback(null);
@@ -333,7 +326,7 @@ function search(){
 				<p>Arcade Fire</p>
 				</a><a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop">Purchase album</a>
 			</li>*/	
-		console.log(video);
+		//console.log(video);
 		
 		html += ('<li data-icon="plus" data-videoid="'+video.videoId+'" ><a onclick="watchVideo(\''+video.videoId+'\');"class="watchVideo" href="#"><img style"vertical-align: middle;" width="120px" height="90px" src="'+video.thumbs[1].url+'" /><h2>'+video.title+'</h2><p><i>'+video.viewCount+' views</i><br/>'+video.description+'</p></a>'+
 			 '<a class="dlLink" href="#" data-role="button" data-rel="dialog" data-transition="pop">Download MP3</a>'+
@@ -356,10 +349,10 @@ function search(){
 		}
 		$(this).simpledialog({
 		    'mode' : 'bool',
-		    'prompt' : 'Are You Sure?',
+		    'prompt' : 'Are you sure?',
 		    'useModal': true,
 		    'buttons' : {
-		      'OK': {
+		      'Download': {
 			click: function () {
 				download($(this), $(this).parent("li").jqmData("videoid"));
 			}
